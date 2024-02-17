814126681addf0226f9f98dd5cdd524390146d18
Bibhu <100408110+DeBibhu562@users.noreply.github.com>
2024-02-17 12:05:42 +0530

    fix issue

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -4,8 +4,8 @@ import EnquiryForm from "../components/EnquiryForm";
 export default function ContactUs() {
   return (
     <div>
-      <h1 className="text-xl font-bold lg:text-2xl text-center">For Connecting with The Education Press mail at :- 
-        <a className="text-lg font-bold lg:text-xl" href="mailto:theeducationpressofficial@gmail.com">
+      <h1 className="text-xl font-bold lg:text-2xl text-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  text-[#fafafa]">For Connecting with The Education Press mail at :- 
+        <a className="text-lg font-bold lg:text-xl " href="mailto:theeducationpressofficial@gmail.com">
           theeducationpressofficial@gmail.com
         </a>
       </h1>
--- client/src/pages/GuestPost.jsx
+++ client/src/pages/GuestPost.jsx
@@ -4,7 +4,7 @@ import EnquiryForm from '../components/EnquiryForm'
 export default function GuestPost() {
   return (
     <div>
-      <h1 className="text-xl font-bold lg:text-2xl text-center">For Connecting with The Education Press mail at :- 
+      <h1 className="text-xl font-bold lg:text-2xl text-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-[#fafafa] ">For Connecting with The Education Press mail at :- 
         <a className="text-lg font-bold lg:text-xl" href="mailto:theeducationpressofficial@gmail.com">
           theeducationpressofficial@gmail.com
         </a>
