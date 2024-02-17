a1045105652e0a10c4153f12b6f28bd06f7fef53
Bibhu <100408110+DeBibhu562@users.noreply.github.com>
2024-02-17 11:38:55 +0530

    fix issue

--- client/src/pages/GuestPost.jsx
+++ client/src/pages/GuestPost.jsx
@@ -4,6 +4,11 @@ import EnquiryForm from '../components/EnquiryForm'
 export default function GuestPost() {
   return (
     <div>
+      <h1 className="text-xl font-bold lg:text-2xl text-center">For Connecting with The Education Press mail at :- 
+        <a className="text-lg font-bold lg:text-xl" href="mailto:theeducationpressofficial@gmail.com">
+          theeducationpressofficial@gmail.com
+        </a>
+      </h1>
       <EnquiryForm />
     </div>
   )
